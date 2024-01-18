c1e63dec894ecf31edf1ffca04c0ec0a013dd166
Phungxuamduc2002 <phungxuanduc2002@gmail.com>
2024-01-18 12:22:53 +0700

    Forms

--- NewApp/obj/Debug/net8.0/NewApp.AssemblyInfo.cs
+++ NewApp/obj/Debug/net8.0/NewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4b110ffa7728e3ef1890adb02ad34fecfde7f79")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99c957e756411fa92e42098f74a936226b1051fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

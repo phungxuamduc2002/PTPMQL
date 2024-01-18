b4b110ffa7728e3ef1890adb02ad34fecfde7f79
Phungxuamduc2002 <phungxuanduc2002@gmail.com>
2024-01-18 10:29:30 +0700

    w3shool

--- NewApp/obj/Debug/net8.0/NewApp.AssemblyInfo.cs
+++ NewApp/obj/Debug/net8.0/NewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16df2db855038ee61f71a3fa1c931288ae1270b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d39836f91fe2a3a2d0b09caff4b6a42ebe4819cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
